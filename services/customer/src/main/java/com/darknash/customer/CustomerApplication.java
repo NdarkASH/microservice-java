40d57641a0b0fa0b255217b3e4b61be63bd4fe0d
darknash <nashihulwan9@gmail.com>
2025-03-07 10:30:13 +0700

    [FIXED]fixed config Customer app

--- services/customer/src/main/java/com/darknash/customer/CustomerApplication.java
+++ services/customer/src/main/java/com/darknash/customer/CustomerApplication.java
@@ -1,4 +1,4 @@
-package com.darknash.ecommerce;
+package com.darknash.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- services/customer/src/test/java/com/darknash/customer/CustomerApplicationTests.java
+++ services/customer/src/test/java/com/darknash/customer/CustomerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.darknash.ecommerce;
+package com.darknash.customer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
