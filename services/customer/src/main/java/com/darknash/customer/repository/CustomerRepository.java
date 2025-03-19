da4cea36426537133846294bccede9b8febb0bb8
darknash <nashihulwan9@gmail.com>
2025-03-19 12:55:05 +0700

    [UPDATE] setting docker composer

--- services/customer/src/main/java/com/darknash/customer/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.darknash.customer.repository;
-
-import com.darknash.customer.document.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-}
